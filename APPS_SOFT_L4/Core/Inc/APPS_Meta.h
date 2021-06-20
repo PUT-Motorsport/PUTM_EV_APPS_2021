0746587728f9fd0165eb2ad090d06b154e9a4b15
Evarios <jan.brozdziak@gmail.com>
2021-06-20 16:42:47 +0200

    rewritten for L4

---
+++ APPS_SOFT_L4/Core/Inc/APPS_Meta.h
@@ -0,0 +1,23 @@
+/*
+ * CAN_meta.h
+ *
+ *  Created on: Nov 23, 2020
+ *      Author: kwitnoncy
+ */
+
+#ifndef INC_APPS_META_H_
+#define INC_APPS_META_H_
+
+#define APPS_CAN_ID 0x0A
+#define APPS_CAN_DLC 2
+
+#define APPS_1_RAW_MIN 1800
+#define APPS_1_RAW_MAX 3050
+
+#define APPS_2_RAW_MIN 2050
+#define APPS_2_RAW_MAX 3430
+
+#define APPS_REAL_MIN 0
+#define APPS_REAL_MAX 500
+
+#endif /* INC_APPS_META_H_ */
